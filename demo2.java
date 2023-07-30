f8b187ecab60b48aa49a2603ce0b3605bc672c6b
Harshil173 <harshilpatadiya8@gmail.com>
2023-07-30 13:47:49 +0530

    First Commit

---
+++ demo2.java
@@ -0,0 +1,12 @@
+class demo2
+{
+        public void hello()
+        {
+            System.out.println("Hello World");
+        }
+        public static void main(String[] args) 
+        {
+            demo2 d1 = new demo2();
+            d1.hello();
+        }
+}
